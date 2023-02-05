37a8d04ec382b2e21a46ff3181562c8d90867c48
k021g1165@g.neec.ac.jp <k021g1165@g.neec.ac.jp>
2023-02-05 23:48:48 +0900

    モデル修正と配列化

--- StageSelectScene.h
+++ StageSelectScene.h
@@ -26,19 +26,28 @@ public:
 
 private:
 
-    DirectX::XMFLOAT2 selecttile1Pos = { 0,232 };
-    DirectX::XMFLOAT2 selecttile2Pos = { 640 ,232 };
-    DirectX::XMFLOAT2 selecttile3Pos = { 1024 ,232 };
+    DirectX::XMFLOAT2 selecttilePos[8] = { {-512,232},{ 0,232 },{ 512 ,232 },{ 1024 ,232 },{ 1536 ,232 },{ 2048 ,232 },{ 2560 ,232 },{ 3072 ,232 } };
+
+    DirectX::XMFLOAT2 selectRope01Pos = { -256 ,328 };
     DirectX::XMFLOAT2 selectRope12Pos = { 256 ,328 };
     DirectX::XMFLOAT2 selectRope23Pos = { 768,328 };
+    DirectX::XMFLOAT2 selectRope34Pos = { 1280,328 };
+    DirectX::XMFLOAT2 selectRope45Pos = { 1792,328 };
+    DirectX::XMFLOAT2 selectRope56Pos = { 2304,328 };
+    DirectX::XMFLOAT2 selectRope67Pos = { 2816,328 };
+
     DirectX::XMFLOAT2 selectpointPos = { 640,360 };
 
-    std::unique_ptr<Sprite> selecttile1 = std::make_unique<Sprite>("Resources/Image/selecttile.png", CMode::PATH);
-    std::unique_ptr<Sprite> selecttile2 = std::make_unique<Sprite>("Resources/Image/selecttile.png", CMode::PATH);
-    std::unique_ptr<Sprite> selecttile3 = std::make_unique<Sprite>("Resources/Image/selecttile.png", CMode::PATH);
+    std::unique_ptr<Sprite> selecttile[8];
 
+
+    std::unique_ptr<Sprite> selectRope01 = std::make_unique<Sprite>("Resources/Image/selectRope.png", CMode::PATH);
     std::unique_ptr<Sprite> selectRope12 = std::make_unique<Sprite>("Resources/Image/selectRope.png", CMode::PATH);
     std::unique_ptr<Sprite> selectRope23 = std::make_unique<Sprite>("Resources/Image/selectRope.png", CMode::PATH);
+    std::unique_ptr<Sprite> selectRope34 = std::make_unique<Sprite>("Resources/Image/selectRope.png", CMode::PATH);
+    std::unique_ptr<Sprite> selectRope45 = std::make_unique<Sprite>("Resources/Image/selectRope.png", CMode::PATH);
+    std::unique_ptr<Sprite> selectRope56 = std::make_unique<Sprite>("Resources/Image/selectRope.png", CMode::PATH);
+    std::unique_ptr<Sprite> selectRope67 = std::make_unique<Sprite>("Resources/Image/selectRope.png", CMode::PATH);
 
     std::unique_ptr<Sprite> selectpoint = std::make_unique<Sprite>("Resources/Image/selectpoint.png", CMode::PATH);
 
